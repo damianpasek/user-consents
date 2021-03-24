33a5a22f5eef7b1411d4cd8fee0ba520d9ac094f
Damian Pasek <damianpasek@gmail.com>
2021-03-24 11:04:29 +0100

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+      diagnostics: false,
+    },
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+}
